c1ce673d801e99b703f405383c817de0ed5e58f7
hub-wen <3159079382@qq.com>
2025-08-24 22:19:00 +0800

    首次提交：多分类微调流程

---
+++ src/App.vue
@@ -0,0 +1,238 @@
+<template>
+  <div>
+
+    <div class="bgGrid">
+      <div class="Grid" v-motion :initial="{ opacity: 0, y: -25 }" :enter="{ opacity: 1, y: 0 }" :duration="1000">
+        <div class="mask"></div>
+        <div class="itemGrid-row" v-for="i in gridRows" :key="i">
+          <div class="itemGrid-cols" v-for="j in gridCols" :key="j"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="main" v-motion :initial="{ opacity: 0 }" :enter="{ opacity: 1 }" :duration="1000">
+      <div class="info">
+        <div class="header">
+          <img src="https://cdn.jsdelivr.net/gh/Hub-wen/blogimage@main/img/202508241846501.jpg" alt="">
+        </div>
+
+        <div class="infoText">
+          <h1>Hi,</h1>
+          <h1>I'm <span class="qn">静燃</span></h1>
+        </div>
+      </div>
+
+      <div class="typewriter">
+        <i class="iconfont icon-baojiaquotation2"></i>
+        <VueTyped :strings="typingTexts" :startDelay="300" :typeSpeed="100" :backSpeed="30" :loop="true"
+          :showCursor="true">
+        </VueTyped>
+        <i class="iconfont icon-baojiaquotation"></i>
+      </div>
+
+      <div class="btns">
+        <a v-for="i in btnList" :key="i.animate" :href="i.href" target="_blank">
+          <vs-button type="gradient" :color="i.color" animation-type="scale">
+            <i :class="`iconfont ${i.icon}`"></i>
+
+            <template #animate>
+              {{ i.animate }}
+            </template>
+          </vs-button>
+        </a>
+
+        <vs-button class="lastBtn" color="#457B9D" animation-type="scale" @click="active = true">
+          <i class="iconfont icon-about"></i>
+
+          <template #animate>
+            关于
+          </template>
+        </vs-button>
+      </div>
+    </div>
+
+    <div class="footer">
+      By Aldebaran from 鹊楠 | ©至今
+    </div>
+
+    <vs-dialog overlay-blur width="550px" not-center v-model="active">
+      <template #header>
+        <h2 class="not-margin">
+          关于本站
+        </h2>
+      </template>
+
+      <div class="con-content">
+
+        <vs-alert color="#00BCD4" type="gradient" v-model:hidden-content="aboutHidden">
+          <template #title>本站基于</template>
+
+          <p>你可以从这里访问 <b>我的博客、GitHub、哔哩哔哩、网易云歌单</b> 以及给我发 <b>邮件</b> ！</p>
+          <p>主页使用了
+            <b><a href="https://quenan.cn/" target="_blank">鹊楠</a></b>
+            主页，感谢awa
+          </p>
+          <p>项目已经开源：</p>
+          <p><a href="https://github.com/QNquenan/homepage-for-vue3"
+              target="_blank">https://github.com/QNquenan/homepage-for-vue3</a>
+          </p>
+        </vs-alert>
+      </div>
+
+      <template #footer>
+        <div class="con-footer">
+          <div class="reTheme">
+
+            <input type="radio" id="light" name="theme" :checked="theme == 'light'">
+            <label @click="setTheme('light')" for="light">
+              <i class="iconfont icon-day"></i>
+            </label>
+
+            <input type="radio" id="system" name="theme" :checked="theme == 'system'">
+            <label @click="setTheme('system')" for="system">
+              <i class="iconfont icon-a-gensuixitong1"></i>
+            </label>
+
+            <input type="radio" id="dark" name="theme" :checked="theme == 'dark'">
+            <label @click="setTheme('dark')" for="dark">
+              <i class="iconfont icon-night"></i>
+            </label>
+
+            <div class="checkedBg"></div>
+          </div>
+
+          <div class="footerBtn">
+            <vs-button color="#ff0000ff" @click="versions" type="border">
+              当前版本
+            </vs-button>
+            <vs-button color="#ff0000ff" @click="active = false">
+              知道啦
+            </vs-button>
+          </div>
+        </div>
+      </template>
+    </vs-dialog>
+  </div>
+</template>
+
+<script>
+import { VsNotification } from 'vuesax-alpha'
+
+export default {
+  data() {
+    return {
+      techHidden: true,
+      aboutHidden: true,
+      typingTexts: [
+        "你好鸭，欢迎来到我的主页！",
+        "彼方尚有荣光在，世界不止眼前的苟且，还有诗和远方",
+        "累了可以在我这里歇歇脚嗷",
+        "May you happy every day",
+      ],
+      btnList: [
+        {
+          icon: 'icon-blogger',
+          animate: '博客',
+          color: '#fe8599',
+          href: 'https://blog.aldebaran.cc'
+        },
+        {
+          icon: 'icon-Githubzhanghao',
+          animate: 'Github',
+          color: '#3d3d3d',
+          href: 'https://github.com/Hub-wen'
+        },
+        {
+          icon: 'icon-Bilibili',
+          animate: 'BiliBili',
+          color: '#0BA6D8',
+          href: 'https://space.bilibili.com/694124492'
+        },
+        {
+          icon: 'icon-E-mail',
+          animate: 'E-mail',
+          color: '#FACB1E',
+          href: 'mailto:3159079382@qq.com'
+        },
+
+      ],
+      avaters: [
+        {
+          color: '#43B884',
+          icon: 'icon-vue',
+          content: 'Vue3'
+        },
+        {
+          color: '#46C93A',
+          icon: 'icon-vuesax-linear-vuesax',
+          content: 'Vuesax for Vue3'
+        },
+        {
+          color: '#1FD0ED',
+          icon: 'icon-less',
+          content: 'Less'
+        },
+        {
+          color: '#FFBA00',
+          icon: 'icon-vite',
+          content: 'Vite'
+        },
+        {
+          color: '#000',
+          icon: 'icon-vercel',
+          content: 'Vercel'
+        },
+        {
+          color: '#000',
+          icon: 'icon-github',
+          content: 'Github'
+        }
+      ],
+      upTime: '2025-03-17',
+      version: 'v3',
+      gridRows: 9,
+      gridCols: 5,
+      active: false,
+      isDarkMode: true,
+      theme: 'system' // 默认是亮色模式
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('isTheme')) {
+      this.theme = localStorage.getItem('isTheme')
+      this.applyTheme()
+    } else {
+      this.applyTheme()
+    }
+    // 监听系统主题变化
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', this.applyTheme);
+  },
+  methods: {
+    setTheme(mode) {
+      this.theme = mode;
+      localStorage.setItem('isTheme', mode)
+      this.applyTheme();
+    },
+    applyTheme() {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      const themeToApply = this.theme === 'system' ? (prefersDark ? 'dark' : 'light') : this.theme;
+      document.documentElement.setAttribute('data-theme', themeToApply);
+    },
+    versions() {
+      VsNotification({
+        icon: '<i class="iconfont icon-guanyu"></i>',
+        progressAuto: true,
+        position: 'top-center',
+        title: '当前的版本',
+        color: '#ff0000ff',
+        content: `现在是 ${this.upTime} 更新的 ${this.version}`
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import url(//at.alicdn.com/t/c/font_4003779_k4bvsymp2l.css);
+/* 阿里巴巴图标库 */
+</style>
